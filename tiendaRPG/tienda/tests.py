f98bd20c61e6e11c94e6cfed1526803ea29938e3
Alvaro Traslaviña <alv.traslavina@alumnos.duoc.cl>
2019-11-27 18:15:02 -0300

    Nuevo test

--- tiendaRPG/tienda/tests.py
+++ tiendaRPG/tienda/tests.py
@@ -4,12 +4,16 @@ from . import models as mod
 
 class PruebaProducto(TestCase):
     def setUp(self):
-        xbox = mod.Productos.objects.create(nombreProducto="Xbox one",precio=1000,puntos = 0,descripcion = 0,stock = 0, urlImagen = "imagen")
+        xbox = mod.Productos.objects.create(nombreProducto="Xbox one",precio=1000,puntos = 5,descripcion = 0,stock = 0, urlImagen = "imagen")
         mod.Recompensa.objects.create(descuento= 50,Producto = xbox,nuevoPrecio = 0,nivel = 5)
 
     def test_asignacion_precio_producto(self):
         Xbox = mod.Productos.objects.get(nombreProducto = "Xbox one")
         self.assertEqual(Xbox.precio,1000)    
+
+    def test_asignacion_puntos_producto(self):
+        Xbox = mod.Productos.objects.get(nombreProducto = "Xbox one")
+        self.assertEqual(Xbox.puntos,5)    
 
     def test_asignacion_descuento(self):
         recompensa = mod.Recompensa.objects.get(nivel = 5)
